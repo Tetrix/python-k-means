a7594212ebdc1f743fac7e44be0cfea52336b56f
shakle17 <saklevaleksandar@gmail.com>
2016-06-20 22:09:45 +0200

    Changed the algorythm, implementation for all files in data

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 This is a python implementation of k-means algorithm
+
+* at the moment the implementation of the algorithm works only for numbers
+* it uses the Euclidean equation , to calculate the distance between the data point and the cluster centroid
+
+------------------------------------------------------------------------------------------------------------
+
+Technologies used:
+
+-python
+-python internal libraries (numpy,csv,math,os)
+
+
+------------------------------------------------------------------------------------------------------------
+
+Scenario:
+
+* In this implementation we use K=2 , which means every data splits into two clusters.
+
+1. Read the the files one by one in the "data" folder, and convert them into lists.
+2. Find optimal centroids for each of the clusters.
+3. Calculate distance between the certain data point and the centroids of the clusters.
+4. Append the data point to the cluster, which has a least distance between it's centroid and the data point
+5. Iterate trough the array (full_list) while there are no changes in the clusters, the algorithm is over.
+
+------------------------------------------------------------------------------------------------------------
+
+Invocation:
+
+- In the (Linux shell/Windows cmd) executre "python k_means.py"
--- k_means.py
+++ k_means.py
@@ -1,68 +1,114 @@
 import numpy
 import csv
-from scipy.spatial import distance
 import math
-from random import randint
-
-filename='data/4clusters.csv'
+import os
 
 
-#Parse the csv file to a list named full_list
+
+#Parse the csv file to a list named full_list ( form a global list of all the data points)
 def parse_csv_to_list(filename):
     full_list = []
 
     with open(filename, 'r') as csvfile:
         csv_reader = csv.reader(csvfile)
         full_list=list(csv_reader)
-    return [map(int,i) for i in full_list]
+    return [map(float,i) for i in full_list]
 
-full_list=parse_csv_to_list(filename)
 
-#Returns number of columns
+
+#Returns number of columns in file beeing executed
 def num_of_lists(full_list):
     return len(full_list[0])
 
-number_of_lists=num_of_lists(full_list)
 
-c1=min(full_list)
-c2=max(full_list)
+# Function used to put the full_list data point in the two clusters correctly
+def form_clusters(c1,c2,cluster,cluster1,cluster2):
+    # Loop , while there is no change for the centroids in any of the clusters
+    while True:
+        for i in range(0,len(full_list)):
+            c1_res=0
+            c2_res=0
+            for j in range(0, number_of_lists):
+                # Calculate Euclid distance between the data point and the centroids of the clusters
+                c1_res = pow(full_list[i][j] - c1[j],2) + c1_res
+                c2_res = pow(full_list[i][j] - c2[j],2) + c2_res
+            c1_res=math.sqrt(c1_res)
+            c2_res=math.sqrt(c2_res)
 
-def form_clusters(column_lists,num_of_lists,c1,c2):
+            c1=min(full_list)
+            c2=max(full_list)
+
+            if c1_res <= c2_res:
+                cluster1.append(full_list[i])
+            else:
+                cluster2.append(full_list[i])
+
+        finish_count=0
+
+        c1=[]
+        c2=[]
+
+        for i in range(0,number_of_lists):
+            c1.append(0)
+            c2.append(0)
+
+        for num in range(0,number_of_lists):
+            for elem in cluster1:
+                c1[num]=c1[num]+elem[num]
+            c1[num]=round(c1[num]/len(cluster1),2)
+
+
+        for num in range(0,number_of_lists):
+            for elem in cluster2:
+                c2[num]=c2[num]+elem[num]
+            c2[num]=round(c2[num]/(len(cluster2)),2)
+
+
+        for i in range(0,len(full_list)):
+            c1_res=0
+            c2_res=0
+            for j in range(0, number_of_lists):
+                c1_res = pow(full_list[i][j] - c1[j],2) + c1_res
+                c2_res = pow(full_list[i][j] - c2[j],2) + c2_res
+            c1_res=round(math.sqrt(c1_res),2)
+            c2_res=round(math.sqrt(c2_res),2)
+
+
+            if ((c1_res <= c2_res) and (full_list[i] in cluster1)) or ((c1_res > c2_res) and (full_list[i] in cluster2)):
+                finish_count=finish_count+1
+                if finish_count==7:
+                    cluster.append(cluster1)
+                    cluster.append(cluster2)
+                    return cluster
+
+        cluster1=[]
+        cluster2=[]
+
+
+
+for test_suite in os.listdir('data'):
+    print "FILE NAME: "+test_suite
+    filename='data/'+test_suite
     cluster=[]
     cluster1=[]
     cluster2=[]
-    c1_res=0
-    c2_res=0
-    #for i in range(1,len(full_list)):
-    for i in range(0,len(full_list)):
-        for j in range(0, number_of_lists):
-            c1_res = pow(full_list[i][j] - c1[j],2) + c1_res
-            c2_res = pow(full_list[i][j] - c2[j],2) + c2_res
-        c1_res=round(math.sqrt(c1_res),2)
-        c2_res=round(math.sqrt(c2_res),2)
 
-        if c1_res <= c2_res:
-            for num in range(0,number_of_lists):
-                element=(full_list[i][num]+c1[num])/2
-                c1[num]=element
-            cluster1.append(full_list[i])
+    full_list=parse_csv_to_list(filename)
+    number_of_lists=num_of_lists(full_list)
 
-        else:
-            for num in range(0,number_of_lists):
-                element=(full_list[i][num]+c2[num])/2
-                c2[num]=element
-            cluster2.append(full_list[i])
-
-        count=count+1
-
-    cluster.append(cluster1)
-    cluster.append(cluster2)
-
-    return cluster
+    c1=min(full_list)
+    c2=max(full_list)
 
 
-clusters=form_clusters(full_list,number_of_lists,c1,c2)
-for c in clusters:
-    print c
-    print "**********"
-    print "**********"
+    clusters=form_clusters(c1,c2,cluster,clster1,cluster2)
+    nm_clusters=0
+
+    for cluster in clusters:
+        nm_clusters=nm_clusters+1
+        print ""
+        print "Cluster nm."+str(nm_clusters)
+        print "*"*20
+        for elements in cluster:
+            print elements
+        print "*"*20
+    print "\n"*5
